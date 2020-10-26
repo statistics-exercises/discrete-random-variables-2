5cc22f7741ce33b8d76b455d4f7eafbf05d787bf
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-26 18:31:45 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ class UnitTests(unittest.TestCase) :
     def test_mean(self) : 
         for i in range(1,9) : 
             p, mean = i*0.1, 0
-            for i in range(100) : mean = mean + bernoulli(0.5)
+            for j in range(100) : mean = mean + bernoulli(0.5)
             mean = mean / 100
             bar = np.sqrt(0.25/100)*st.norm.ppf( (0.99 + 1) / 2 )
             self.assertTrue( np.fabs( mean - p )<bar, "The function does not appear to be generating the random variable correctly" )

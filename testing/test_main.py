4b467a2611f49d443464dfe9853fb597d58da8dd
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 09:43:30 +0100

    Fixed tests to use autofeedback

---
+++ testing/test_main.py
@@ -0,0 +1,22 @@
+try:
+    from AutoFeedback.funcchecks import check_func 
+except:
+    import subprocess
+    import sys
+            
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "AutoFeedback"])
+    from AutoFeedback.funcchecks import check_func 
+           
+from AutoFeedback.randomclass import randomvar
+import unittest
+from main import *
+
+class UnitTests(unittest.TestCase) :
+    def test_mean(self) : 
+        inputs, variables = [], []
+        for i in range(1,9) :
+            p = i*0.1
+            inputs.append((i*0.1,))
+            myvar = randomvar( p, variance=p*(1-p), vmin=0, vmax=1, isinteger=True )
+            variables.append( myvar )
+        assert( check_func('bernouli',inputs, variables ) )

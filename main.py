e593fa657483b8424e4e91fa708917f2b4b44b86
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-11 09:45:20 +0100

    Added answer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def bernoulli(p):
    # Your code goes here
-
+   if np.random.uniform(0,1)<p : return 1
+   return 0
 
 print( bernoulli(0.5), bernoulli(0.5), bernoulli(0.5)  )
